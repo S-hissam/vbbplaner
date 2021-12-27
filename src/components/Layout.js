cf3bfd40d227f18b2f2abfe5e7d37c42e1e18e3b
Salahuddin Hissam <salahuddin.hissamuddin@gmail.com>
2021-12-27 07:20:36 +0100

    size

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -8,7 +8,7 @@ export default function Layout({ children }) {
           <div className="mockup-phone drop-shadow-2xl">
             <div className="camera"></div>
             <div className="display">
-              <div className="artboard phone-4 artboard-demo justify-start bg-gradient-to-r from-cyan-500 to-blue-500">
+              <div className="artboard phone-3 artboard-demo justify-start bg-gradient-to-r from-cyan-500 to-blue-500">
                 {children}
                 <div className="navbar shadow-lg bg-black text-neutral-content w-full absolute bottom-2 flex items-center justify-around " >
                   <a href='/' className="hover:animate-pulse btn btn-ghost rounded-btn">
